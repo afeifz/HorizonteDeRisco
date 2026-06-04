851caec072fb6527f255bf52d15cf12d6c7e2af5
Mohamed <mohamedafifsmaili@gmail.com>
2026-06-04 00:23:22 -0300

    Streaming the project!!!!!!

---
+++ src/main/java/br/com/global/HorizonteDeRiscoApplication.java
@@ -0,0 +1,13 @@
+package br.com.global;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonteDeRiscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonteDeRiscoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/global/HorizonteDeRiscoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.global;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonteDeRiscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
